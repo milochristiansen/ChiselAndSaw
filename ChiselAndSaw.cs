67f6d382573383ad4282da015b1c2db1748d9324
Milo Christiansen <milo.christiansen@gmail.com>
2018-03-29 14:56:48 -0400

    Split the mesher off into its own class.

--- ChiselAndSaw.cs
+++ ChiselAndSaw.cs
@@ -9,8 +9,8 @@ using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
 
-namespace VSExampleMods {
-	public class chiselandsaw : ModBase {
+namespace ChiselAndSaw {
+	public class ChiselAndSaw : ModBase {
 		public override void Start(ICoreAPI api) {
 			api.RegisterItemClass("ItemChisel", typeof(ItemChisel));
 			api.RegisterBlockClass("BlockChisel", typeof(BockChisel));
@@ -161,7 +161,7 @@ namespace VSExampleMods {
 			var mesh = ModelCache.Get(itemstack.Attributes.GetInt("meshid"), capi.Render, capi.World);
 			if (mesh == null) {
 				var block = capi.World.GetBlock((ushort)itemstack.Attributes.GetInt("blockid"));
-				var mdata = BlockEntityChisel.GenInventoryMesh(itemstack.Attributes.GetBytes("voxels"), capi, block);
+				var mdata = VoxelMesher.GenInventoryMesh(itemstack.Attributes.GetBytes("voxels"), capi, block);
 				mesh = capi.Render.UploadMesh(mdata);
 				int mid = ModelCache.New(mesh, capi.Render, capi.World);
 				itemstack.Attributes.SetInt("meshid", mid);
@@ -472,204 +472,9 @@ namespace VSExampleMods {
 			selectionBoxes = boxes.ToArray();
 		}
 
-		public static MeshData GenInventoryMesh(byte[] data, ICoreClientAPI capi, Block block) {
-			if (data == null || data.Length < 16 * 16 * 16 / 8) {
-				return null;
-			};
-			var mesh = genMesh(new BitArray(data), capi, block);
-			mesh.rgba2 = null;
-			return mesh;
-		}
-
 		public void RegenMesh() {
 			ICoreClientAPI capi = api as ICoreClientAPI;
-			mesh = genMesh(voxels, capi, SrcBlock);
-		}
-
-		private static MeshData genMesh(BitArray voxels, ICoreClientAPI capi, Block block) {
-			var mesh = new MeshData(24, 36, false).WithTints().WithRenderpasses();
-
-			// The New! Shiny! Inefficent! version.
-			// Dynamically generating the needed quads is a step along the path to greedy meshing though.
-			bool[] sideVisible = new bool[6];
-			for (int x = 0; x < 16; x++) {
-				for (int y = 0; y < 16; y++) {
-					for (int z = 0; z < 16; z++) {
-						if (!voxels[at(x, y, z)]) continue;
-
-						sideVisible[0] = z == 0 || !voxels[at(x, y, z - 1)];
-						sideVisible[1] = x == 15 || !voxels[at(x + 1, y, z)];
-						sideVisible[2] = z == 15 || !voxels[at(x, y, z + 1)];
-						sideVisible[3] = x == 0 || !voxels[at(x - 1, y, z)];
-						sideVisible[4] = y == 15 || !voxels[at(x, y + 1, z)];
-						sideVisible[5] = y == 0 || !voxels[at(x, y - 1, z)];
-
-						for (int f = 0; f < 6; f++) {
-							if (!sideVisible[f]) continue;
-							mesh.AddMeshData(GenQuad(f, x, y, z, 1, 1, capi, block));
-						}
-					}
-				}
-			}
-			return mesh;
-		}
-
-		private static int[][] coordIndexByFace = new int[][] {
-			// N
-			new int[] { 0, 1 },
-			// E
-			new int[] { 2, 1 },
-			// S
-			new int[] { 0, 1 },
-			// W
-			new int[] { 2, 1 },
-			// U
-			new int[] { 0, 2 },
-			// D
-			new int[] { 0, 2 }
-		};
-
-		private static int[] flippedCords = new int[] { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-
-		private static MeshData GenQuad(int face, int x, int y, int z, int w, int h, ICoreClientAPI capi, Block block) {
-			var shading = (byte)(255 * CubeMeshUtil.DefaultBlockSideShadingsByFacing[face]);
-
-			// I'm pretty sure this isn't correct for quads larger than w=1,h=1.
-			// Larger quads probably need to be translated one way or another.
-
-			// North: Negative Z
-			// East: Positive X
-			// South: Positive Z
-			// West: Negative X
-
-			var u = 1 / 16f;
-			var hu = 1 / 32f;
-			var xf = x * u;
-			var yf = y * u;
-			var zf = z * u;
-			var wf = w * u;
-			var hf = h * u;
-			MeshData quad;
-			switch (face) {
-				case 0: // N
-					quad = QuadMeshUtil.GetCustomQuad(xf, yf, zf + u, wf, hf, 255, 255, 255, 255);
-					quad.Rotate(new Vec3f(xf + hu, yf + hu, zf + hu), 0, GameMath.PI, 0);
-					break;
-				case 1: // E
-					quad = QuadMeshUtil.GetCustomQuad(xf, yf, zf + u, wf, hf, 255, 255, 255, 255);
-					quad.Rotate(new Vec3f(xf + hu, yf + hu, zf + hu), 0, GameMath.PIHALF, 0);
-					break;
-				case 2: // S
-					quad = QuadMeshUtil.GetCustomQuad(xf, yf, zf + u, wf, hf, 255, 255, 255, 255);
-					break;
-				case 3: // W
-					quad = QuadMeshUtil.GetCustomQuad(xf, yf, zf + u, wf, hf, 255, 255, 255, 255);
-					quad.Rotate(new Vec3f(xf + hu, yf + hu, zf + hu), 0, -GameMath.PIHALF, 0);
-					break;
-				case 4: // U
-					quad = QuadMeshUtil.GetCustomQuadHorizontal(xf, yf, zf, wf, hf, 255, 255, 255, 255);
-					quad.Rotate(new Vec3f(xf + hu, yf + hu, zf + hu), GameMath.PI, 0, 0);
-					break;
-				default: // D
-					quad = QuadMeshUtil.GetCustomQuadHorizontal(xf, yf, zf, wf, hf, 255, 255, 255, 255);
-					break;
-			}
-
-			quad.Rgba = new byte[16];
-			quad.Rgba.Fill(shading);
-			quad.rgba2 = new byte[16];
-			quad.rgba2.Fill(shading);
-			quad.Flags = new int[4];
-			quad.Flags.Fill(0);
-			quad.RenderPasses = new int[1];
-			quad.RenderPassCount = 1;
-			quad.Tints = new int[1];
-			quad.TintsCount = 1;
-			quad.XyzFaces = new int[] { face };
-			quad.XyzFacesCount = 1;
-
-
-			float subPixelPadding = capi.BlockTextureAtlas.SubPixelPadding;
-			TextureAtlasPosition tpos = capi.BlockTextureAtlas.GetPosition(block, BlockFacing.ALLFACES[face].Code);
-			for (int j = 0; j < quad.Uv.Length; j++) {
-				quad.Uv[j] = (j % 2 > 0 ? tpos.y1 : tpos.x1) + quad.Uv[j] * 32f / capi.BlockTextureAtlas.Size - subPixelPadding;
-			}
-
-			int[] coords = new int[]{
-				x, y, z,
-			};
-
-			// Flip the UVs as needed. Screws up rotation even more than it already is, but we need to fix that anyway.
-			int ox = coords[coordIndexByFace[face][0]];
-			int oy = coords[coordIndexByFace[face][1]];
-			switch (face) {
-				case 0: // N
-					ox = flippedCords[ox];
-					oy = flippedCords[oy];
-					break;
-				case 1: // E
-					ox = flippedCords[ox];
-					oy = flippedCords[oy];
-					break;
-				case 2: // S
-					oy = flippedCords[oy];
-					break;
-				case 3: // W
-					oy = flippedCords[oy];
-					break;
-				case 4: // U
-					ox = flippedCords[ox];
-					oy = flippedCords[oy];
-					break;
-				default: // D
-					ox = flippedCords[ox];
-					break;
-			}
-
-			float offsetX = (ox * 2f) / capi.BlockTextureAtlas.Size;
-			float offsetZ = (oy * 2f) / capi.BlockTextureAtlas.Size;
-			for (int i = 0; i < quad.Uv.Length; i += 2) {
-				quad.Uv[i] += offsetX;
-				quad.Uv[i + 1] += offsetZ;
-			}
-
-			switch (face) {
-				case 0: // N
-					SwapUV(ref quad, 0, 6);
-					SwapUV(ref quad, 2, 4);
-					break;
-				case 1: // E
-					SwapUV(ref quad, 0, 6);
-					SwapUV(ref quad, 2, 4);
-					break;
-				case 2: // S
-					SwapUV(ref quad, 0, 6);
-					SwapUV(ref quad, 2, 4);
-					break;
-				case 3: // W
-					SwapUV(ref quad, 0, 6);
-					SwapUV(ref quad, 2, 4);
-					break;
-				case 4: // U
-					SwapUV(ref quad, 0, 2);
-					SwapUV(ref quad, 4, 6);
-					break;
-				default: // D
-					SwapUV(ref quad, 0, 2);
-					SwapUV(ref quad, 4, 6);
-					break;
-			}
-
-			return quad;
-		}
-
-		private static void SwapUV(ref MeshData quad, int a, int b) {
-			var x = quad.Uv[a];
-			var y = quad.Uv[a + 1];
-			quad.Uv[a] = quad.Uv[b];
-			quad.Uv[a + 1] = quad.Uv[b + 1];
-			quad.Uv[b] = x;
-			quad.Uv[b + 1] = y;
+			mesh = VoxelMesher.GenBlockMesh(voxels, capi, SrcBlock);
 		}
 
 		private static int at(int x, int y, int z) {
--- README.md
+++ README.md
@@ -16,3 +16,4 @@ TODO:
 * Block matrial and other properties should be based on the source block type (needs two chiseled block varients, wood and stone).
 * Greedy meshing.
 * Compress the voxel data.
+* All the voxel model stuff should be moved to its own class.
